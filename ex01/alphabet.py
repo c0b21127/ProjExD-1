81e681e0f0026245ed64e9e114c0d2d72b29a844
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 16:50:19 +0900

    alphabet

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -15,6 +15,20 @@ def shutudai():
     print(ra)
 
 
+def kaito(a):
+    ans1 = input('欠損文字の数は？')
+    if ans1 == kes_alp:
+        print('正解です。欠損文字を答えてください。 ')
+        while True:
+            ans2 = input('1つ目の文字は？')
+            if ans2 != a:
+                print('不正解です。もう一度チャレンジしてください。')
+        
+        print('正解!!!')
+    else:
+        print('不正解です。また、チャレンジしてください。')
+
+
 if __name__ == "_main_":
     alp = ['A','B','C','D','E','F','G','H','I','J','K',
 'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
