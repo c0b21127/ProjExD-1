0fd4a6cfb5cb790901565a594e144b5650e88d6b
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 16:21:34 +0900

    alphabet

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,10 +1,20 @@
 import random
 
+full_alp = 26
+tai_alp = 10
+kes_alp = 2
+try_alp = 2
 
 def shutudai():
-    r = random.sample(taishou, 10)
+    r = random.sample(alp, tai_alp)
+    print('対象文字:')
     print(r)
 
+    ra = random.samle(r, kes_alp)
+    print('欠損文字:')
+    print(ra)
+
+
 if __name__ == "_main_":
-    taishou = ['A','B','C','D','E','F','G','H','I','J','K',
+    alp = ['A','B','C','D','E','F','G','H','I','J','K',
 'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
