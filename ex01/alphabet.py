311c3ccddb32148c184cc080d055f72c73aeea3e
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 17:52:50 +0900

    alphabet #2

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 alp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 full_alp = 26 # 全アルファベット数
@@ -6,11 +7,10 @@ tai_alp = 10 # 対象文字数
 kes_alp = 2 # 欠損文字数
 try_alp = 2 # チャレンジできる回数
 
-
+# 全アルファベットから，対象文字をランダムに10文字選ぶ
 r = random.sample(alp, tai_alp)
 print('対象文字:')
 print(r)
-
 
 ra = random.sample(r, kes_alp)
 print('欠損文字:')
