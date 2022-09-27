9984b25041b45b7622f8c6e3761ab073de5f5755
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 16:11:05 +0900

    alphabet

---
+++ ex01/alphabet.py
@@ -0,0 +1,10 @@
+import random
+
+
+def shutudai():
+    r = random.sample(taishou, 10)
+    print(r)
+
+if __name__ == "_main_":
+    taishou = ['A','B','C','D','E','F','G','H','I','J','K',
+'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
