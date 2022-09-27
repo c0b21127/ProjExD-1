be91182e243f72b80a11336f81354f7e4cab0343
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 15:40:42 +0900

    サザエさんクイズ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,13 +1,19 @@
 import random
-quiz = {'サザエの旦那の名前は？':['ますお','マスオ'],'カツオの妹の名前は？':['わかめ','ワカメ'],
-'タラオはカツオから見てどんな関係？':['甥','おい','甥っ子','おいっこ']}
 
-select1, select2 = random.choice(list(quiz.items()))
+def shutudai(select1) :
 
-print('問題:')
-print(select1)
-answer = input('答えるんだ:')
-if answer in select2:
-    print('正解!!!')
-else:
-    print('出直してこい')
+    print('問題:')
+    print(select1)
+
+def kaito(select2):
+    answer = input('答えるんだ:')
+    if answer in select2:
+        print('正解!!!')
+    else:
+        print('出直してこい')
+
+if __name__ == "_main_":
+    quiz = {'サザエの旦那の名前は？':['ますお','マスオ'],'カツオの妹の名前は？':['わかめ','ワカメ'],
+    'タラオはカツオから見てどんな関係？':['甥','おい','甥っ子','おいっこ']}
+
+    select1, select2 = random.choice(list(quiz.items()))
