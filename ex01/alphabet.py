b6b35d0f1147e7ff82233b1b32a7ec6db83877de
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-09-27 17:51:15 +0900

    alphabet #1

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,19 +1,20 @@
 import random
 
 alp = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-full_alp = 26
-tai_alp = 10
-kes_alp = 2
-try_alp = 2
+full_alp = 26 # 全アルファベット数
+tai_alp = 10 # 対象文字数
+kes_alp = 2 # 欠損文字数
+try_alp = 2 # チャレンジできる回数
 
 
 r = random.sample(alp, tai_alp)
 print('対象文字:')
 print(r)
 
+
 ra = random.sample(r, kes_alp)
 print('欠損文字:')
-print(ra,end=" ")
+print(ra)
 
 
 ans1 = input('欠損文字の数は？')
