488530405353958d5aecbd321fed4c8cd1e79cf8
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-10-04 14:30:37 +0900

    calc.py

---
+++ ex02/calc.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+root = tk.Tk()
+root.title("title")
+root.geometry("300x500")
+
+r, c = 0, 0
+for i, num in enumerate(range(9,-1, -1), 1):
+    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.grid(row=r, column=c)
+    c += 1
+    if i%3 == 0:
+        r += 1
+        c = 0
+
+root.mainloop()
