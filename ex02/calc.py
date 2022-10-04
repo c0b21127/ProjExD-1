f9ef93d6741c7ceed3fbd37f8dce5db2114805ae
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-10-04 15:19:54 +0900

    練習6

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,7 +6,7 @@ from turtle import right
 
 def button_click(event):
     btn = event.widget
-    txt = int(btn["text"])
+    txt = btn["text"]
     tkm.showinfo(f"{txt}",f"{txt}のボタンが押されました")
     entry.insert(tk.END,txt)
 
@@ -19,7 +19,7 @@ entry.grid(row=0,column=0,columnspan=3)
 
 r, c = 1, 0
 for i, num in enumerate(range(9,-1, -1), 1):
-    btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width=4, height=2)
+    btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width=4, height=1)
     btn.bind("<1>",button_click)
     btn.grid(row=r, column=c)
     c += 1
@@ -27,5 +27,7 @@ for i, num in enumerate(range(9,-1, -1), 1):
         r += 1
         c = 0
 
-
+btn1 = tk.Button(root, text="+", font=("Times New Roman", 30), width=4, height=1)
+btn1.grid(row = 4, column= 1 )
+btn1.bind("<1>",button_click)
 root.mainloop()
