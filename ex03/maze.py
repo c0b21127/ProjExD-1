107fd6c446bad4fa031760bf1bb65425dfe65cff
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-10-18 16:53:07 +0900

    追加機能

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,7 +13,7 @@ def key_up(event):
 
 def main_proc():
     global cx, cy
-    global mx, my
+    global mx, my 
     if key == "Up":
         my -= 1
     elif key == "Down":
@@ -33,7 +33,7 @@ def main_proc():
             mx += 1
         if key == "Right":
             mx -= 1
-    canv.coords("tori",cx, cy)         
+    canv.coords("tori",cx, cy,)   
     root.after(100, main_proc)
 
 if __name__ == "__main__":
@@ -45,14 +45,14 @@ if __name__ == "__main__":
 
     maze_list = maze_maker.make_maze(15,9)
     maze_maker.show_maze(canv,maze_list)
-
+    print(maze_list)
     
-
-    tori = tk.PhotoImage(file="fig/6.png")
+    tori = tk.PhotoImage(file="pika.png")
     cx, cy = 300, 400
     mx, my = 1,1
-    canv.create_image(cx, cy, image=tori, tag = "tori") 
+    canv.create_image(cx, cy, image=tori, tag = "tori")
 
+     
     key = "" #グローバル変数keyは、現在押されているキーを表す変数である #練習4
 
     root.bind("<KeyPress>",key_down) #練習5
