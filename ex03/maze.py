f9cfe7828b33db87cc1ad2eb9e7755facbb35b9c
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-10-18 14:43:20 +0900

    練習7

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,28 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Left":
+        cx -= 20
+    elif key == "Right":
+        cx += 20   
+    canv.coords("tori",cx, cy)         
+    root.after(100, main_proc)
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -10,4 +33,12 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/6.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag = "tori") 
+
+    key = "" #グローバル変数keyは、現在押されているキーを表す変数である #練習4
+
+    root.bind("<KeyPress>",key_down) #練習5
+
+    root.bind("<KeyRelease>", key_up) #練習6
+
+    main_proc()
     root.mainloop()
