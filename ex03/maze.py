fe039500d4ccc9a8c56a5c2c878c79a42e2cfa2d
ikeda motoki <c0b2101536@edu.teu.ac.jp>
2022-10-18 14:17:57 +0900

    練習3

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,4 +6,8 @@ if __name__ == "__main__":
 
     canv = tk.Canvas(root,width = 1500,height = 900, bg= "black")
     canv.pack()
+
+    tori = tk.PhotoImage(file="fig/6.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag = "tori") 
     root.mainloop()
